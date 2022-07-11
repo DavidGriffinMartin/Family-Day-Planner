83724f7dcebf7d826d9065554f0cad29b7491944
Dave Martin <105015655+DavidGriffinMartin@users.noreply.github.com>
2022-07-11 17:18:23 -0400

    TESTING

---
+++ main_app/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
+
+
+class Event(models.Model):
+    date = models.DateField()
+    name = models.CharField(max_length=100)
+    timeBeg = models.TimeField(auto_now=False, auto_now_add=False)
+    timeEnd = models.TimeField(auto_now=False, auto_now_add=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+      
+    def get_absolute_url(self):
+        return reverse('dashboard')
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
+from .models import Event
+from .forms import DateForm
 
 
 def home(request):
@@ -15,10 +21,28 @@ def dashboard(request):
     return render(request, 'dashboard.html')
 
 
+# def pick_date (request):
+#     date = DateForm(request)
+#     return redirect('date_detail.html')
+
+
+def date_detail(request):
+    if request.method == 'POST':
+        form = request.POST
+        # form.save()
+        # date = request.GET.get('date')
+        print(form, "hello")
+        events = Event.objects.all()
+        return render(request, 'date_detail.html', {'events': events})
+    else:
+        return render(request, 'about.html')
+
+
 def signup(request):
     error_message = ''
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
+        print(form)
         try:
             user = form.save()
             login(request, user)
@@ -29,3 +53,13 @@ def signup(request):
     form = UserCreationForm()
     context = {'form': form, 'error_message': error_message}
     return render(request, 'registration/signup.html', context)
+
+
+class EventCreate(LoginRequiredMixin, CreateView):
+    model = Event
+    fields = ['date', 'name', 'timeBeg', 'timeEnd']
+    success_url = '/dashboard/'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
