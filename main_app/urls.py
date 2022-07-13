6a363645d1e15e50685738465122a8eb41d5d29e
Dave Martin <105015655+DavidGriffinMartin@users.noreply.github.com>
2022-07-13 19:23:13 -0400

    wip: Adds EventUpdate path.

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,4 +1,3 @@
-from django.contrib import admin
 from django.urls import path, include
 
 from . import views
@@ -10,8 +9,9 @@ urlpatterns = [
     path('profile/<int:user_id>/', views.users_profile, name='users_profile'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('event/create/', views.EventCreate.as_view(), name='event_create'),
+    path('event/<int:pk>/update/', views.EventUpdate.as_view(), name='event_update'),
+    path('event/<int:pk>/delete/', views.EventDelete.as_view(), name='event_delete'),
     path('dashboard/date_detail/', views.date_detail, name='date_detail'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/signup/', views.signup, name='signup'),
-    path('event/<int:pk>/delete/', views.EventDelete.as_view(), name='event_delete'),
 ]
